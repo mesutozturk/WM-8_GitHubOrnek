d59522c1d1dcbdc9887cdfb6d65ce9b6be255595
Dogukansvnc <101670829+Dogukansvnc@users.noreply.github.com>
2022-04-06 12:11:21 +0300

    Doğukan

--- GitHubOrnek.Console/Program.cs
+++ GitHubOrnek.Console/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+<<<<<<< HEAD
 Console.WriteLine("Doğukan");
+=======
+Console.WriteLine("Mesut");
+Console.WriteLine("Mert");
+Console.WriteLine("Ahmet");
+>>>>>>> c2f183457a17a26d3e8dd5248c8604a53ff8bf97
