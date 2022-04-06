d07343d18e312e3cc8cf1b6deb35a5253b9002fa
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-04-06 13:49:42 +0300

    Update Program.cs

--- GitHubOrnek.Console/Program.cs
+++ GitHubOrnek.Console/Program.cs
@@ -1,12 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+<<<<<<< HEAD
 Console.WriteLine("Can");
 Console.WriteLine("Sema");
 Console.WriteLine("Mert");
 Console.WriteLine("Murat");
+=======
+>>>>>>> parent of 0ee8ebd (Çaça)
 Console.WriteLine("Emrecan");
 Console.WriteLine("Ali Erhan");
 Console.WriteLine("Mesut");
+Console.WriteLine("Mert");
 Console.WriteLine("Ahmet");
 Console.WriteLine("Can");
 Console.WriteLine("Yağızhan");
@@ -19,7 +23,6 @@ Console.WriteLine("Talha");
 Console.WriteLine("C");
 Console.WriteLine("A");
 Console.WriteLine("N");
-Console.WriteLine("Talha");
 Console.WriteLine("Talha");
 Console.WriteLine("Sema");
 Console.WriteLine("Doğukan");
